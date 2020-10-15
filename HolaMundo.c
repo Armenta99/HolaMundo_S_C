cd1874c559d9973fa6d457f113794d9578d7af5e
José Francisco Armenta <60667433+Armenta99@users.noreply.github.com>
2020-10-15 15:11:38 -0700

    Add files via upload
    Agregué mis Hola mundo

---
+++ HolaMundo.c
@@ -0,0 +1,13 @@
+/* Armenta Peña José Francisco */
+/* No. Control: 18212146*/
+/* Programa en ARM64: Hola mundo*/
+/* Materia: Lenguajez de Interfaz*/
+
+
+
+#include <stdio.h>
+int main (void)
+{
+	printf("Hola Mundo Cruel y Despiadado\n");
+	return 0;
+}
